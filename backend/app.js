68ca7542f2d7e6a67bdfbe6bb832c3c0b9c596bc
Dluser3 <Satyamtiwai.dl@gmail.com>
2024-03-12 12:40:22 +0530

    creating build

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,7 @@ const patientRoutes = require("./routes/patient.routes");
 const departmentRoutes = require("./routes/deparment.routes");
 const problemRoutes = require("./routes/problem.routes");
 const cookieParser = require("cookie-parser");
-
+const path = require("path");
 const app = express();
 app.use(cookieParser());
 app.use(express.json());
@@ -20,8 +20,16 @@ app.use("/api/patient", patientRoutes);
 app.use("/api/department", departmentRoutes);
 app.use("/api/problem", problemRoutes);
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
+app.use("/", express.static(path.join(__dirname, "/build")));
+app.get("/", function (req, res) {
+  return res.sendFile(path.join(__dirname, "/build/index.html"));
 });
+app.get("/*", function (req, res) {
+  return res.sendFile(path.join(__dirname, "/build/index.html"));
+});
+
+// app.get("/", (req, res) => {
+//   res.send("Hello world");
+// });
 
 module.exports = app;
--- backend/config/db.js
+++ backend/config/db.js
@@ -9,6 +9,7 @@ mongoose.connection.on("end", () => {
 });
 
 const url = "mongodb://patientDb_user:pwd-PATIENTDB56@localhost:27017/patientdb";
+// const url = "mongodb://localhost:27017/patientdb";
 
 const startDatabase = async () => {
   try {
